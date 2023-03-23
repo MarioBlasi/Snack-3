6532d6892527b5635dc2bf4448e86d7de87013da
MarioBlasi <mblasi509@gmail.com>
2023-03-23 11:58:06 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.*/
+
+const animali = [
+  { nome: "leone", famiglia: "felidi", classe: "mammiferi" },
+  { nome: "cane", famiglia: "canidi", classe: "mammiferi" },
+  { nome: "gallina", famiglia: "fasianidi", classe: "uccelli" },
+  { nome: "Leone", famiglia: "Felidi", classe: "Mammiferi" },
+  { nome: "Orso", famiglia: "Ursidi", classe: "Mammiferi" },
+  { nome: "Tartaruga", famiglia: "Testugginidi", classe: "Rettili" },
+];
+
+console.log(animali);
